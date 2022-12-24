ab722153e79606b8a67519b2ff6375e88c3b3c41
Aatir Fayyaz <88738828+aatirfayyaz@users.noreply.github.com>
2022-12-23 23:04:14 -0500

    Initial commit
    Files for the retrieval of data from Google Big Query

---
+++ Data Retrieval from GBQ/google_auth_pred.py
@@ -0,0 +1,22 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import pandas as pd
+import time
+
+credentials = service_account.Credentials.from_service_account_file('/path/to/json/for/GCPBigQueryAccess')
+
+project_id = 'project-name-in-gcp'
+client = bigquery.Client(credentials= credentials, project=project_id)
+
+while True:
+    query_job = client.query("""
+    SELECT *
+    FROM project-ID-dataset.table_name
+    LIMIT 1000 """)
+
+    print("Retrieving data from big query ......")
+    results = query_job.to_dataframe() # Wait for the job to complete.
+    
+    results.to_csv("pred.csv")
+    print("Data saved to csv successfully! ")
+    time.sleep(10)
